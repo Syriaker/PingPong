6fa41e1150c200d913d5130985ed8a417714ca87
Syriaker <chernetsov1130@gmail.com>
2023-05-14 16:06:45 +0300

    added win contitions

--- PingPong.py
+++ PingPong.py
@@ -1,5 +1,8 @@
 from pygame import *
 
+score_1 = 0
+score_2 = 0
+timer = 120
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed, speed_y=0, speed_x=0, angle=0):
         super().__init__()
@@ -30,14 +33,30 @@ class Player(GameSprite):
             self.rect.y += self.speed
 class Ball(GameSprite):
     def move(self, win):
+        global timer
         win.blit(self.image, (self.rect.x, self.rect.y))
-        self.rect.x += self.speed_x
-        self.rect.y += self.speed_y
-        if self.rect.y < 0 or self.rect.y > win_height - self.size_y:
-            self.speed_y *= -1
+        if timer <= 0:
+            self.rect.x += self.speed_x
+            self.rect.y += self.speed_y
+            if self.rect.y < 0 or self.rect.y > win_height - self.size_y:
+                self.speed_y *= -1
     def collide(self, player_racket1, player_racket2):
         if sprite.collide_rect(self, player_racket1) or sprite.collide_rect(self, player_racket2):
             self.speed_x *= -1
+    def win_condition(self):
+        global score_1
+        global score_2
+        global timer
+        if self.rect.x > win_width - self.size_x:
+            score_1 += 1
+            self.rect.x = 350
+            self.rect.y = 250
+            timer = 120
+        if self.rect.x < 0:
+            score_2 += 1
+            self.rect.x = 350
+            self.rect.y = 250
+            timer = 120
 
 
 win_width, win_height = 800,600
@@ -47,6 +66,10 @@ background = transform.scale(image.load ("field.jpg"), (win_width, win_height))
 FPS = 60
 clock = time.Clock()
 run = True
+finish = False
+font.init()
+font1 = font.SysFont('Impact', 40)
+font2 = font.SysFont('Impact', 32)
 racket1 = Player('platform.jpg', 0, 0, 25, 150, 5)
 racket2 = Player('platform.jpg', win_width-25, 0, 25, 150, 5)
 ball = Ball('ball.png', 350,250,50,50, 0, 3,3)
@@ -54,12 +77,25 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-    window.blit(background, (0, 0))
-    racket1.draw(window)
-    racket2.draw(window)
-    ball.move(window)
-    ball.collide(racket1,racket2)
-    racket1.move_l()
-    racket2.move_r()
-    clock.tick(FPS)
-    display.update()
+    if not finish:
+        window.blit(background, (0, 0))
+        win1 = font1.render('ИГРОК 1 ПОБЕДИЛ!', True, (50,205,50))
+        win2 = font1.render('ИГРОК 2 ПОБЕДИЛ!', True, (50,205,50))
+        score = font2.render(str(score_1) + ':' + str(score_2), True, (255,255,255))
+        racket1.draw(window)
+        racket2.draw(window)
+        ball.move(window)
+        ball.collide(racket1,racket2)
+        racket1.move_l()
+        racket2.move_r()
+        ball.win_condition()
+        window.blit(score, (380,0))
+        if score_1 >= 3:
+            window.blit(win1, (275,250))
+            finish = True
+        if score_2 >= 3:
+            window.blit(win2, (275,250))
+            finish = True
+        timer -= 1
+        clock.tick(FPS)
+        display.update()
